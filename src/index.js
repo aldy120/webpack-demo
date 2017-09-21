2770ad0c74c823cfc5484cae56d928e15df76e6c
lichi.chen <lichi.chen@lichichende-MacBook-Pro.local>
2017-09-21 10:24:31 +0800

    Load data from xml and csv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import Image400200 from './pic400200.jpeg';
+import csvData from './data.csv';
+import xmlData from './data.xml';
 
 function component() {
   var element = document.createElement('div');
@@ -10,6 +12,10 @@ function component() {
   var myImage = new Image();
   myImage.src = Image400200;
   element.appendChild(myImage);
+
+  console.log(xmlData);
+  console.log(csvData);
+
 
   return element;
 }
